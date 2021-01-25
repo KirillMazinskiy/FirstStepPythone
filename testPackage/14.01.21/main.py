c181aff8a24fde608c46c6fde5df8bfed757eddd
Kirill Mazinski <chikkahikka@mail.ru>
2021-01-25 18:32:31 +0300

    Lgfm

--- testPackage/14.01.21/main.py
+++ testPackage/14.01.21/main.py
@@ -1,5 +1,5 @@
 def main():
-    z1()
+    z2()
 
 def z1():
     worker = ['Olya', 'Silyutina', 350000, 6]
@@ -15,4 +15,21 @@ def z1():
         status = user_name +" "+ user_family + " is " + position
         print(status)
 
+def z2():
+    workers = [['Ivan', 'Ivanov', 100000, 7], ['Petr', 'Petrov', 150000, 2], ['Sidor', 'Sidorov', 200000, 3]]
+    for i in range(len(workers)):
+            status = '123'
+            user_name = workers[i][0]
+            user_family = workers[i][1]
+            position = '123'
+            if workers[i][3] < 2:
+                position = 'junior'
+            elif (workers[i][3] >= 2 and workers[i][3] <= 5) :
+                position = 'middle'
+            elif workers[i][3] > 5 :
+                position = 'senior'
+            status = user_name +" "+ user_family + " is " + position
+            print(status)
+
 main()
+
